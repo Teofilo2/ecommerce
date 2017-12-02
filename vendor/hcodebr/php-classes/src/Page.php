4d76c7251cda5dbfc70b3adadc89cbc64f65a67a
Teofilo Laurentino <ps04591@gmail.com>
2017-12-02 18:33:46 -0200

    Tela de Login

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -9,10 +9,14 @@ class Page {
 	private $tpl;
 	private $options = [];
 	private $defaults = [
+		"header"=>true,
+		"footer"=>true,
 		"data"=>[]
 	];
 
 	public function __construct($opts = array(), $tpl_dir = "/views/") {
+
+		//$this->defaults["data"]["session"] = $_SESSION;
 
 		$this->options = array_merge($this->defaults, $opts);
 
@@ -28,8 +32,11 @@ class Page {
 		
 		$this->setData($this->options["data"]);
 
-		$this->tpl->draw("header");
+		if ($this->options["header"] === true) {
+				
+			$this->tpl->draw("header");
 
+		}
 	}
 
 	private function setData($data = array()) {
@@ -50,7 +57,11 @@ class Page {
 
 	public function __destruct() {
 
-		$this->tpl->draw("footer");
+		if ($this->options["footer"] === true) {
+				
+			$this->tpl->draw("footer");
+
+		}
 
 	}
 
