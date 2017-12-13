549f92d92940b5faddd8c43aca1982321deea74f
Teofilo Laurentino <ps04591@gmail.com>
2017-12-13 18:43:15 -0200

    Site Login

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -10,6 +10,8 @@ class User extends Model {
 
 	const SESSION = "User";
 	const SECRET = "HcodePhp7_Secret";
+    const ERROR = "UserError";
+    const ERROR_REGISTER = "UserErrorRegister";
 
     public static function getFromSession()
     {
@@ -62,9 +64,9 @@ class User extends Model {
 
 		$sql = new Sql();
 
-		$results = $sql->select("SELECT * FROM tb_users WHERE deslogin = :LOGIN", array(
-			":LOGIN"=>$login
-		));
+		$results = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b ON a.idperson = b.idperson WHERE a.deslogin = :LOGIN", array(
+            ":LOGIN"=>$login
+        ));
 
 		if (count($results) === 0) 
 		{
@@ -77,6 +79,8 @@ class User extends Model {
 		{
 
 			$user = new User();
+
+            $data['desperson'] = utf8_encode($data['desperson']);
 
 			$user->setData($data);
 
@@ -93,12 +97,16 @@ class User extends Model {
 	public static function verifyLogin($inadmin = true) 
 	{
 
-		if (User::checkLogin($inadmin)) {
+		if (!User::checkLogin($inadmin)) {
 
-			header("Location: /admin/login");
-			exit;
-
-		}
+            if ($inadmin) {
+                header("Location: /admin/login");
+            } else {
+                header("Location: /login");
+            }
+            exit;
+    	
+        }
 
 	}
 
@@ -124,9 +132,9 @@ class User extends Model {
     	$sql = new Sql();
 
     	$results = $sql->select("CALL sp_users_save(:desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
-    		":desperson"=>$this->getdesperson(),
+    		":desperson"=>utf8_decode($this->getdesperson()),
     		":deslogin"=>$this->getdeslogin(),
-    		":despassword"=>$this->getdespassword(),
+    		":despassword"=>User::getPasswordHash($this->getdespassword()),
     		":desemail"=>$this->getdesemail(),
     		":nrphone"=>$this->getnrphone(),
     		":inadmin"=>$this->getinadmin()
@@ -160,9 +168,9 @@ class User extends Model {
 
     	$results = $sql->select("CALL sp_usersupdate_save(:iduser, :desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
     		":iduser"=>$this->getiduser(),
-    		":desperson"=>$this->getdesperson(),
+    		":desperson"=>utf8_decode($this->getdesperson()),
     		":deslogin"=>$this->getdeslogin(),
-    		":despassword"=>$this->getdespassword(),
+    		":despassword"=>User::getPasswordHash($this->getdespassword()),
     		":desemail"=>$this->getdesemail(),
     		":nrphone"=>$this->getnrphone(),
     		":inadmin"=>$this->getinadmin()
@@ -297,6 +305,78 @@ class User extends Model {
             ":password"=>$password,
             ":iduser"=>$this->getiduser()
         ));
+
+    }
+
+    public static function setError($msg) 
+    {
+
+        $_SESSION[User::ERROR] = $msg;
+
+    }
+
+    public static function getError()
+    {
+
+        $msg = (isset($_SESSION[User::ERROR]) && $_SESSION[User::ERROR]) ? $_SESSION[User::ERROR] : '';
+
+        User::clearError();
+
+        return $msg; 
+
+    }
+
+    public static function clearError() 
+    {
+
+        $_SESSION[User::ERROR] = NULL;
+
+    }
+
+    public static function setErrorRegister($msg) 
+    {
+
+        $_SESSION[User::ERROR_REGISTER] = $msg;
+
+    }
+
+    public static function getErrorRegister()
+    {
+
+        $msg = (isset($_SESSION[User::ERROR_REGISTER]) && $_SESSION[User::ERROR_REGISTER]) ? $_SESSION[User::ERROR_REGISTER] : '';
+
+        User::clearErrorRegister();
+
+        return $msg;
+
+    }
+
+    public function clearErrorRegister()
+    {
+
+        $_SESSION[User::ERROR_REGISTER] = NULL;
+
+    }
+
+    public static function checkLoginExist($login)
+    {
+
+        $sql = new Sql();
+
+        $results = $sql->select("SELECT * FROM tb_users WHERE deslogin = :deslogin", [
+            ':deslogin'=>$login
+        ]);
+
+        return (count($results) > 0);
+
+    }
+
+    public static function getPasswordHash($password)
+    {
+
+        return password_hash($password, PASSWORD_DEFAULT, [
+            'cost'=>12
+        ]);
 
     }
 
